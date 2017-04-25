f0ffeb46d391b1f0466d6164c2f30430b612bb8c
Todd Way <toddway@gmail.com>
2017-04-24 23:05:44 -0500

    fix facebook view

--- login/src/main/java/com/vml/login/FacebookLoginView.java
+++ login/src/main/java/com/vml/login/FacebookLoginView.java
@@ -2,6 +2,7 @@ package com.vml.login;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.util.Log;
 
 import com.facebook.CallbackManager;
@@ -11,6 +12,7 @@ import com.facebook.FacebookSdk;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -21,42 +23,25 @@ import java.util.Arrays;
  * Created by tway on 12/5/16.
  */
 
-public class FacebookLoginView implements ThirdPartyLoginView {
+public class FacebookLoginView implements ThirdPartyLoginView, FacebookCallback<LoginResult> {
 
     private CallbackManager callbackManager;
     private Activity activity;
+    LoginResult listener;
 
     public FacebookLoginView(Activity activity, String facebookAppId) {
         this.activity = activity;
-        this.callbackManager = CallbackManager.Factory.create();
         FacebookSdk.setApplicationId(facebookAppId);
         FacebookSdk.sdkInitialize(activity.getApplicationContext());
+        this.callbackManager = CallbackManager.Factory.create();
+        LoginManager.getInstance().registerCallback(callbackManager, this);
+
     }
 
     @Override
-    public void showLogin(final LoginResult listener) {
-        LoginManager.getInstance().registerCallback(CallbackManager.Factory.create(), new FacebookCallback<com.facebook.login.LoginResult>() {
-            @Override
-            public void onSuccess(final com.facebook.login.LoginResult loginResult) {
-                GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
-                    @Override
-                    public void onCompleted(JSONObject object, GraphResponse response) {
-                        listener.onLoginResult(mapToLoginData(loginResult.getAccessToken().getToken(), object));
-                    }
-                });
-            }
-
-            @Override
-            public void onCancel() {
-                listener.onLoginResult(null);
-            }
-
-            @Override
-            public void onError(FacebookException error) {
-                listener.onLoginError(error.getMessage());
-            }
-        });
-        LoginManager.getInstance().logInWithReadPermissions(activity, Arrays.asList("email"));
+    public void showLogin(LoginResult listener) {
+        this.listener = listener;
+        LoginManager.getInstance().logInWithReadPermissions(activity, Arrays.asList("public_profile", "email", "user_birthday"));
     }
 
     public void handleActivityResult(int requestCode, int resultCode, Intent data) {
@@ -78,5 +63,30 @@ public class FacebookLoginView implements ThirdPartyLoginView {
             Log.e("", e.getMessage());
             return null;
         }
+    }
+
+    @Override
+    public void onSuccess(final com.facebook.login.LoginResult loginResult) {
+        GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response) {
+                listener.onLoginResult(mapToLoginData(loginResult.getAccessToken().getToken(), object));
+            }
+        });
+
+        Bundle parameters = new Bundle();
+        parameters.putString("fields","id,name,email,gender,birthday");
+        request.setParameters(parameters);
+        request.executeAsync();
+    }
+
+    @Override
+    public void onCancel() {
+        listener.onLoginResult(null);
+    }
+
+    @Override
+    public void onError(FacebookException error) {
+        listener.onLoginError(error.getMessage());
     }
 }
